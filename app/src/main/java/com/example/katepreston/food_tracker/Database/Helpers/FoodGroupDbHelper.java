c47fcc51de21d48451885935fd307e2b0ac02956
Kate Preston <ktweeden@googlemail.com>
2018-03-24 10:43:54 +0000

    Created FoodGroup dbHelper with basic crud actions. Close #8

--- app/src/main/java/com/example/katepreston/food_tracker/Database/Helpers/FoodGroupDbHelper.java
+++ app/src/main/java/com/example/katepreston/food_tracker/Database/Helpers/FoodGroupDbHelper.java
@@ -1,8 +1,89 @@
 package com.example.katepreston.food_tracker.Database.Helpers;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.katepreston.food_tracker.Database.Contracts.FoodGroupContract;
+import com.example.katepreston.food_tracker.Database.DbHelper;
+import com.example.katepreston.food_tracker.Models.FoodGroup;
+
 /**
  * Created by katepreston on 24/03/2018.
  */
 
-public class FoodGroupDbHelper {
+public class FoodGroupDbHelper extends DbHelper {
+
+    public FoodGroupDbHelper(Context context) {
+        super(context);
+    }
+
+    public void save(FoodGroup foodGroup) {
+        ContentValues values = new ContentValues();
+        SQLiteDatabase db = this.getWritableDatabase();
+        values.put(FoodGroupContract.COLUMN_NAME_NAME, foodGroup.getName());
+
+        foodGroup.setId(db.insert(FoodGroupContract.TABLE_NAME, null, values));
+    }
+
+    public void update(FoodGroup foodGroup) {
+        ContentValues values = new ContentValues();
+        SQLiteDatabase db = this.getWritableDatabase();
+        values.put(FoodGroupContract.COLUMN_NAME_NAME, foodGroup.getName());
+
+        String whereClause = FoodGroupContract._ID + " = ?";
+        String[] whereArgs = {foodGroup.getId().toString()};
+
+        db.update(FoodGroupContract.TABLE_NAME, values, whereClause, whereArgs);
+    }
+
+    public void delete(FoodGroup foodGroup) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String whereClause = FoodGroupContract._ID + " = ?";
+        String[] whereArgs = {foodGroup.getId().toString()};
+
+        db.delete(FoodGroupContract.TABLE_NAME, whereClause, whereArgs);
+    }
+
+    public static Cursor findByid(Long id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String[] columns = {
+                FoodGroupContract._ID,
+                FoodGroupContract.COLUMN_NAME_NAME
+        };
+
+        String whereClause = FoodGroupContract._ID + " = ?";
+        String[] whereArgs = {id.toString()};
+
+        Cursor cursor = db.query(
+                FoodGroupContract.TABLE_NAME,
+                columns,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return cursor;
+    }
+
+    public static Cursor findAll() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String[] columns = {
+                FoodGroupContract._ID,
+                FoodGroupContract.COLUMN_NAME_NAME
+        };
+
+        Cursor cursor = db.query(
+                FoodGroupContract.TABLE_NAME,
+                columns,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        return cursor;
+    }
 }
--- app/src/main/java/com/example/katepreston/food_tracker/Models/FoodGroup.java
+++ app/src/main/java/com/example/katepreston/food_tracker/Models/FoodGroup.java
@@ -4,12 +4,23 @@ package com.example.katepreston.food_tracker.Models;
  * Created by katepreston on 23/03/2018.
  */
 
-public enum FoodGroup {
-    DAIRY,
-    MEAT,
-    FISH,
-    FRUIT,
-    VEGETABLES,
-    SWEETS,
-    STARCHES
+public class FoodGroup {
+    private String name;
+    private Long id;
+
+    public FoodGroup(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
