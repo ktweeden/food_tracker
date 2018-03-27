dafa2e6b7aadd5bff6a78ebd95173fb1e2fc5ecf
Kate Preston <ktweeden@googlemail.com>
2018-03-27 15:10:39 +0100

    main activity is expandable list view of meals and foods. Closes #26

--- app/src/main/java/com/example/katepreston/food_tracker/Activities/MainActivity.java
+++ app/src/main/java/com/example/katepreston/food_tracker/Activities/MainActivity.java
@@ -5,37 +5,61 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
 import com.example.katepreston.food_tracker.Adaptors.FoodAdaptor;
+import com.example.katepreston.food_tracker.Adaptors.MealAdaptor;
 import com.example.katepreston.food_tracker.Database.Helpers.FoodDbHelper;
+import com.example.katepreston.food_tracker.Database.Helpers.MealDbHelper;
 import com.example.katepreston.food_tracker.Database.Helpers.SeedDbHelper;
 import com.example.katepreston.food_tracker.Models.Food;
+import com.example.katepreston.food_tracker.Models.Meal;
 import com.example.katepreston.food_tracker.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Meal> meals;
+    private HashMap<Meal, ArrayList<Food>> foods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SeedDbHelper.seed(this);
-        FoodDbHelper foodDbHelper = new FoodDbHelper(this);
-        FoodAdaptor foodAdaptor = new FoodAdaptor(this, foodDbHelper.findAll());
-        ListView listview = findViewById(R.id.food_list_view);
-        listview.setAdapter(foodAdaptor);
+        prepareListData();
+
+        ExpandableListView listView = findViewById(R.id.meal_list);
+        MealAdaptor mealAdaptor = new MealAdaptor(this, this.meals, this.foods);
+        listView.setAdapter(mealAdaptor);
+
     }
 
-    public void onAddNewFoodClick(View listFoods) {
+    public void onAddNewMealClick(View listFoods) {
         Intent intent = new Intent(this, AddMealActivity.class);
 
         startActivity(intent);
     }
 
-    public void onSingleFoodItemClick(View foodItem) {
-        Food selectedFood = (Food) foodItem.getTag();
-        Intent intent = new Intent(this, SingleFoodActivity.class);
-        intent.putExtra("food",selectedFood);
-        startActivity(intent);
+    public void prepareListData() {
+        MealDbHelper mealDbHelper = new MealDbHelper(this);
+        FoodDbHelper foodDbHelper = new FoodDbHelper(this);
+        this.meals = mealDbHelper.findAll();
+        this.foods = new HashMap<>();
+
+        for (Meal meal : this.meals) {
+            ArrayList<Food>foodList = foodDbHelper.findByMealid(meal.getId());
+            this.foods.put(meal, foodList);
+        }
     }
+
+//    public void onSingleFoodItemClick(View foodItem) {
+//        Food selectedFood = (Food) foodItem.getTag();
+//        Intent intent = new Intent(this, SingleFoodActivity.class);
+//        intent.putExtra("food",selectedFood);
+//        startActivity(intent);
+//    }
 }
---
+++ app/src/main/java/com/example/katepreston/food_tracker/Adaptors/MealAdaptor.java
@@ -0,0 +1,115 @@
+package com.example.katepreston.food_tracker.Adaptors;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.example.katepreston.food_tracker.Database.Helpers.FoodGroupDbHelper;
+import com.example.katepreston.food_tracker.Models.Food;
+import com.example.katepreston.food_tracker.Models.Meal;
+import com.example.katepreston.food_tracker.Models.Utils;
+import com.example.katepreston.food_tracker.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by katepreston on 27/03/2018.
+ */
+
+public class MealAdaptor extends BaseExpandableListAdapter {
+
+    private Context context;
+    private ArrayList<Meal> meals;
+    private HashMap<Meal, ArrayList<Food>> foods;
+
+    public MealAdaptor(Context context, ArrayList<Meal> meals, HashMap<Meal, ArrayList<Food>> foods) {
+        this.context = context;
+        this.meals = meals;
+        this.foods = foods;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return meals.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return foods.get(meals.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.meals.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return foods.get(meals.get(groupPosition)).get(childPosititon);
+    }
+
+    @Override
+    public long getGroupId(int position) {
+        return position;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPostion, boolean isExpanded, View convertView, ViewGroup parent) {
+        Meal currentMeal = (Meal) getGroup(groupPostion);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(this.context).inflate(R.layout.single_meal, parent, false);
+        }
+
+        TextView name = convertView.findViewById(R.id.meal_list_name);
+        name.setText(currentMeal.getName());
+        TextView date = convertView.findViewById(R.id.meal_list_date);
+        date.setText(Utils.dateToString(currentMeal.getDate()));
+        TextView rating = convertView.findViewById(R.id.meal_list_rating);
+        rating.setText(currentMeal.getRating().name());
+
+        convertView.setTag(currentMeal);
+        return convertView;
+
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        Food currentFood = (Food) getChild(groupPosition, childPosition);
+        FoodGroupDbHelper foodGroupDbHelper = new FoodGroupDbHelper(this.context);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(this.context).inflate(R.layout.single_food, parent, false);
+        }
+
+        if (currentFood != null) {
+            TextView name = convertView.findViewById(R.id.food_name);
+            name.setText(currentFood.getName());
+
+            TextView foodGroup = convertView.findViewById(R.id.single_food_foodgroup);
+            foodGroup.setText(foodGroupDbHelper.findByid(currentFood.getFoodGroup()).get(0).getName());
+
+            convertView.setTag(currentFood);
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
